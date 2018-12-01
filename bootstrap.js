11d728358d5ec84f312f2ac4551c8bd7c697b343
Vijay Nair <vijay@dffrnt.com>
2018-12-01 19:38:19 +0530

    Added new extension, bootstrap, which runs user's custom JavaScript snippet on each game start.

---
+++ bootstrap.js
@@ -0,0 +1,36 @@
+!function() {
+	 let settings = {
+		bootstrapCode: "",
+	}
+
+	function settingsApplied(values) {
+		settings = values;
+	}
+
+	function createSettingsProvider() {
+		let sp = new SettingsProvider(settings, settingsApplied)
+	
+		let section = sp.addSection("Bootstrap");
+
+		section.addString("bootstrapCode",
+			"Run this code when game starts. My personal favorite is turning off missile smoke particles to reduce a bit of CPU load.",
+			{maxLength: 200})
+
+		return sp
+	}
+	
+	SWAM.on("gameRunning", function() {
+		// For some reason, settings.bootstrapCode did not work. Have to fetch directly from LocalStorage instead oTL
+		setTimeout(JSON.parse(localStorage.SWAM_Settings).extensions.yutru.bootstrap.bootstrapCode
+, 0)
+	})
+
+	SWAM.registerExtension({
+		name: "Bootstrap",
+		id: "yutru.bootstrap",
+		description: "Runs JavaScript code snippet of your choice as soon as the game starts. Great for stuff like customizing your flag or changing game engine settings. My personal favorite is turning off missile smoke particles to reduce a bit of CPU load.",
+		author: "yutru",
+		version: "1.0.0",
+		settingsProvider: createSettingsProvider()
+	})
+}()
